ceed3dc4e3a374f5356330c559eb7444f909e566
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-08-06 21:45:42 +0530

    Added Milestone Assessment 3 Qs

---
+++ src/Milestone_3/Milestone_Assessment_3/Sum_of_Sum_Digits_Cycle.java
@@ -0,0 +1,18 @@
+/*
+    Program to find the sum of sum of difits in a number in a cycle.
+
+    Input: 582109
+    Sum of digits = ('5' + '8' + '2' + '1' + '0' + '9') + ('8' + '2' + '1' + '0' + '9') + ('2' + '1' + '0' + '9') + ('1' + '0' + '9')
+                    + ('0' + '9') + (9)
+                  = 85
+    Output: 85
+
+    Input: 1001
+    Output: 5
+ */
+
+package Milestone_3.Milestone_Assessment_3;
+
+public class Sum_of_Sum_Digits_Cycle {
+
+}
---
+++ src/Milestone_3/Milestone_Assessment_3/UserId_Generation.java
@@ -0,0 +1,23 @@
+/*
+  Complete the given function to generate a User ID for an online gaming competition that takes 4 arguments - first name, last name, PIN and a digit N.
+
+  Rule 1: The smaller length name is taken as Smaller_Name and the larger name is taken as Larger_Name. If the two names are of equal length,
+  then the name which comes first in alphabetical order is taken as Smaller_Name while the other name is taken as Larger_Name.
+
+  Rule 2: The user ID is formed as follows:
+          last letter of Smaller_Name + whole Larger_Name + Nth digit of PIN read from Left to Right + Nth digit of PIN from Right to Left.
+
+  Rule 3: Once the above format is constructed, toggle the letters i.e. Lowercase letters are converted to Capital, and vice-versa.
+          Return the resultant String as the user ID.
+
+    Sample Input: Rajiv, Roy, 564859, 2
+    Sample Output: YrAJIV65
+
+ */
+package Milestone_3.Milestone_Assessment_3;
+
+public class UserId_Generation {
+    public static void main(String[] args) {
+
+    }
+}
